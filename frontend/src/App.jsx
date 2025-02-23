c575f95cef8e04273c5fbca296f7b5fd5ec26ce4
babasting123 <ben55368755@gmail.com>
2025-02-23 22:16:07 +0700

    :D

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,32 @@
-import Layout from "./components/Layouts/layout"
-import UploadZone from "./components/Pages/NewDesign/upload-zone"
-import DocumentGrid from "./components/Pages/NewDesign/document-grid"
-import ActionButtons from "./components/Pages/NewDesign/action-buttons"
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layouts/layout";
+import ActionButtons from "./components/Pages/NewDesign/action-buttons";
+import UploadZone from "./components/Pages/NewDesign/upload-zone";
+import DocumentGrid from "./components/Pages/NewDesign/document-grid";
+import Login from "./components/Pages/Login";
+import Register from "./components/Pages/Register";
 
-function App() {
-
+const App = () => {
   return (
-    <Layout>
-      <ActionButtons />
-      <UploadZone />
-      <div className="mt-8">
-        <DocumentGrid />
-      </div>
-    </Layout>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/dashboard"
+          element={
+            <Layout>
+              <ActionButtons />
+              <UploadZone />
+              <div className="mt-8">
+                <DocumentGrid />
+              </div>
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
