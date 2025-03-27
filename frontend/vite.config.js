ad77ee38d95c6ab6a617ec6a77567716be40f9ec
AterixGz <128670686+AterixGz@users.noreply.github.com>
2025-03-27 21:13:08 +0700

    CreateFolder

--- frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['@tailwindcss/oxide-win32-x64-msvc']
-  }
-})
