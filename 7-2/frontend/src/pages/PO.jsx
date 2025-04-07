103ad03455b5d86c4d871dec7d4209bf21eafa47
babasting123 <ben55368755@gmail.com>
2025-04-08 05:36:32 +0700

    e

--- 7-2/frontend/src/pages/PO.jsx
+++ 7-2/frontend/src/pages/PO.jsx
@@ -757,7 +757,7 @@ const PRSelectionModal = ({ approvedPRs, onSelect, onClose }) => {
 
 const VendorSelectionModal = ({ vendors, onSelect, onClose }) => {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 background-sudHod flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-3/4 max-h-[80vh] overflow-y-auto">
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-xl font-medium">เลือกผู้ขาย</h2>
